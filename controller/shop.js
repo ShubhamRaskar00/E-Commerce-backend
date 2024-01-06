60b5bf0bd852cc9163403ef83edfe13bd9c9e4ce
Shubham Raskar <106386589+ShubhamRaskar00@users.noreply.github.com>
2024-01-06 19:35:45 +0530

    Update shop.js

--- controller/shop.js
+++ controller/shop.js
@@ -39,7 +39,7 @@ router.post("/create-shop", catchAsyncErrors(async (req, res, next) => {
 
     const activationToken = createActivationToken(seller);
 
-    const activationUrl = `https://patilorganics.vercel.app/activation/${activationToken}`;
+    const activationUrl = `https://patilorganics.vercel.app/seller/activation/${activationToken}`;
 
     try {
       await sendMail({
